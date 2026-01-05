b2bc283d35c6047da69f1a902f828df48f91ebf6
Anthony Gyabaah <gyabaaanthony1@gmail.com>
2026-01-04 18:27:44 -0800

    different functions to handle both signals while introducing the modulated signal but it's not working out. will try a different approach later

--- Amplitude Modulation/main.cpp
+++ Amplitude Modulation/main.cpp
@@ -6,9 +6,8 @@
 #include <numbers>
 
 
-int main(){
-
-std::cout<<"Input the signal. Format:(A 'cos||sin' w) ";
+void carrierSignal(){
+    std::cout<<"Input the signal. Format:(A 'cos||sin' w) ";
 std::string inputCarrierSignal;
 std::getline(std::cin,inputCarrierSignal);  //user enters the signal here
 
@@ -20,14 +19,14 @@ float omega=0;
 std::stringstream ss(inputCarrierSignal);  //so this is where the the various
 if(!(ss>>amplitude>>functionType>>omega)){   // Erorr  //variables will be extracted from the input using the stringstream function
     std::cerr<<"Invalid input\n";        
-    return -1;
+    
 }
 float pi=3.14159265f;
 float frequency=omega/ (2*pi);
 
 const int windowWidth=800;
 const int windowHeight=400;             //seting my window size already
-const float sampleFrequency=100.f;
+const float sampleFrequency=1000000.f;
 
 std::vector<sf::Vector2f> points;      //the Vector2f function actually repressent a point on the screen which will be ideal to plot (x,)
 points.reserve(800);                 //best if the gpu allocate some bit before hand
@@ -76,9 +75,115 @@ window.draw(wave);
 window.display();
 
 
+}
+}
+
+
+
+void modulatingSignal(){
+                           //so this will also use the same aproach. a well 
+    std::cout<<"Enter the modulating/message signal\n";  //class can be used to make this 
+    std::string inputMessageSignal;                     //very simple and easier
+    std::getline(std::cin,inputMessageSignal);
+
+    float amplitude=0;
+    std::string functionType;
+    float omega=0;
+
+    //extracting the three components from the buffer
+
+    std::stringstream ss(inputMessageSignal);
+    if(!(ss>>amplitude>>functionType>>omega)){                //if the function doesn't extract it in this manner, then it's an error
+        std::cerr<<"wrong input, checkout the format well\n";  //incase
+    }
+
+    //working on the math now
+
+    const int windowWidth=600;
+    const int windowHeight=500;
+    float sampleFrequency=2000.f;
+
+    std::vector <sf::Vector2f> points2;
+    points2.reserve(600);
+    
+    for(int i=0;i<windowWidth;i++){
+        float t=(float)i / sampleFrequency;
+        float y=0;
+
+        if(functionType=="cos"){
+            y=amplitude* std::cos(omega*t);
+        }
+        else if(functionType=="sin"){
+            y=amplitude*std::sin(omega*t);
+        }
+
+        //now, since the base values are set, the subsequent values must also be added
+        points2.push_back(sf::Vector2f((float)i,(windowHeight/2.f)-y));
+
+    }
+         //getting a line to be drawn between two different point2 elements for all the elements
+    sf::VertexArray wave2(sf::LineStrip,points2.size());
+
+    //since points2 cannot simply be drawn out easily, it should point towards wave2
+    //wave2 is just housing it
+     for(int i=0;i<points2.size();i++){
+        wave2[i].position=points2[i];
+        wave2[i].color=sf::Color::Cyan;
+     }
+
+
+   sf::RenderWindow window(sf::VideoMode({windowWidth,windowHeight}),"Modulating signal");
+   
+   while(window.isOpen()){
+
+      sf::Event event;
+
+         while(window.pollEvent(event)){
+            if(event.type==sf::Event::Closed){
+                window.close();
+            }
+         }
+
+         window.clear(sf::Color::Green);
+         window.draw(wave2);
+         window.display();
+
+
+
+
+   }
 
 
 }
+
+
+
+
+
+
+
+
+
+int main(){
+
+carrierSignal();
+modulatingSignal();
+std::cout<<"\n The modulated signal will be displayed using the square law modulator\n";
+
+float a=1.0f;
+float b=0.5f;
+
+for(int i=0;i<windowWidth;i++){
+    float ct=inputCarrierSignal;
+    float mt=inputMessageSignal;
+    std::vector<sf::Vector2f> modulatedPoints;
+
+    float v2= a*inputCarrierSignal + 2*b*inputCarrierSignal*inputMessageSignal;
+  modulatedPoints.push_back(sf::Vector2f((float)i,(windowWidth/2.f)-v2));
+}
+
+
+
 
     return 0;
 }
