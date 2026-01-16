c9c5eb65e5899ee837ca4abb9127907a9645f017
Anthony Gyabaah <gyabaaanthony1@gmail.com>
2026-01-16 08:35:27 -0800

    A successful project from viewing both signals to calculations
    Integrated two classes to handle the calculations

--- Amplitude Modulation/main.cpp
+++ Amplitude Modulation/main.cpp
@@ -173,6 +173,267 @@ window.display();
 
 
 
+class multipleCalculation{    // hoping this will handle the calculations when its a multitone signal
+
+public:
+
+struct multipleTones{       // a struct to hold the data for each tone
+    std::string functionType;
+    double amplitude;
+    double omega;
+};
+
+multipleTones carrier, tone1, tone2, tone3;  // defining the carrier and 3 tones
+
+// now we need to take the input from the user and split it into individual tones
+void getInput(){
+
+std::cout<<"\nEnter the modulated signal in the format: Ac [ 1 + u1 func W1t + u2 func W2t + u3 func W2t ] func Wct. The 't' is not important\n ";
+std::string input;
+std::getline(std::cin, input);
+std::cin.clear();  // clearing the input buffer
+
+// now using the same logic as before, hoping it'll work out
+std::stringstream hugeStream(input);
+std::string firstSplit;  // this is the will split it at the ']' character
+
+std::getline(hugeStream, firstSplit, ']');  // get the first part of the string
+hugeStream >> carrier.functionType >> carrier.omega; // Carrier is what's left in hugeStream
+std::string firstPart; // these will hold the two parts of the string
+ // split at the ']' character
+
+
+// now we have the two parts, we can process them
+
+
+
+
+
+// now we need to process the first part to get the tones
+// removing the 'Ac [ ' part
+
+std::stringstream hugeStream2(firstSplit);
+std::string gettingAc;  // getting the value of Ac from the firstPart
+std::getline(hugeStream2, gettingAc, '['); 
+std::stringstream toneStream2(gettingAc);
+toneStream2 >> carrier.amplitude; // Get Ac from the part before '['
+
+std::string insideTheBrackets;  // this will hold the inside of the brackets
+
+std::getline(hugeStream2, insideTheBrackets);
+
+// now we have the inside of the brackets, we can process it to get the tones
+
+std::stringstream hugeStream3(insideTheBrackets);
+std::string individualTones;
+
+int i=0; // since it needs to loop through the bracket four times
+int carrierConstant=1; // to skip the first part which is always 1
+while(std::getline(hugeStream3,individualTones,'+') && i<4){
+    
+    std::stringstream toneStream3(individualTones);
+
+    if(i==0){
+        carrierConstant=1; // just to skip the first part
+    }
+    else if(i==1){
+        toneStream3>>tone1.amplitude>>tone1.functionType>>tone1.omega;
+    }
+    else if(i==2){
+        toneStream3>>tone2.amplitude>>tone2.functionType>>tone2.omega;
+    }
+    else if(i==3){
+        toneStream3>>tone3.amplitude>>tone3.functionType>>tone3.omega;
+    }
+    else{
+     std::cerr<<"Invalid input\n";     // do nothing
+     std::cout<<"Exiting...\n";
+     std::cin.ignore();
+     exit(0);
+    }
+
+i++;
+}
+
+
+
+}
+
+
+void display(){
+
+
+    float u1 = tone1.amplitude;
+    float u2 = tone2.amplitude;
+    float u3 = tone3.amplitude;
+
+    float modulationIndex = sqrt((u1*u1)+(u2*u2)+(u3*u3));
+    float carrierPower =(carrier.amplitude*carrier.amplitude)/2.f;
+    float sideBandPower = (carrierPower/2.f)*(modulationIndex*modulationIndex);
+    float totalPower = carrierPower + sideBandPower;
+    float efficiency = (sideBandPower/totalPower) * 100.f ;   
+
+
+    std::cout<<"Just to be sure! this is the wave\n";  // displaying the wave
+    std::cout<<"AM= "<<carrier.amplitude<<" [ 1 + "<<tone1.amplitude<<"*"<<tone1.functionType<<"*"<<tone1.omega<<"t + "<<tone2.amplitude<<"*"<<tone2.functionType<<"*"<<tone2.omega<<"t + "<<tone3.amplitude<<"*"<<tone3.functionType<<"*"<<tone3.omega<<"t ] * "<<carrier.functionType<<"*"<<carrier.omega<<"t\n";
+    
+    std::cout<<"\nThe various parameters are as follows:\n";
+    std::cout<<"Carrier Frequency: "<<(carrier.omega/(2*3.14159))<<"\n";
+    std::cout<<"There are 6 bands in total, 3 USBs and 3 LSBs\n";
+    std::cout<<"Modulating index of the first tone: "<<tone1.amplitude<<"\n";
+    std::cout<<"Modulating index of the second tone: "<<tone2.amplitude<<"\n";
+    std::cout<<"Modulating index of the third tone: "<<tone3.amplitude<<"\n";
+    std::cout<<"Total Modulating index: "<<modulationIndex<<"\n";
+    std::cout<<"Carrier Power: "<<carrierPower<<" W\n";
+    std::cout<<"Sideband Power: "<<sideBandPower<<" W\n";
+    std::cout<<"Total Power: "<<totalPower<<" W\n";
+    std::cout<<"Efficiency: "<<efficiency<<" %\n";
+
+    // now on the various bands
+    std::cout<<"The various bands are as follows:\n";
+    std::cout<<"Frequency of USB1: "<<(carrier.omega/(2*3.14159))+(tone1.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of LSB1: "<<(carrier.omega/(2*3.14159))-(tone1.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of USB2: "<<(carrier.omega/(2*3.14159))+(tone2.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of LSB2: "<<(carrier.omega/(2*3.14159))-(tone2.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of USB3: "<<(carrier.omega/(2*3.14159))+(tone3.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of LSB3: "<<(carrier.omega/(2*3.14159))-(tone3.omega/(2*3.14159))<<"\n";
+
+std::cin.clear();
+std::cin.get();
+
+}
+
+
+multipleTones carrier1, tone1_1, tone2_1;  // defining the carrier and 2 tones for the 2 tone version
+void getInput1(){   // manually create this to display the 2 tone version, since trying to make it dynamic will be tedious
+
+
+std::cout<<"\nEnter the modulated signal in the format: Ac [ 1 + u1 func W1t + u2 func W2t ] func Wct. The 't' is not important\n ";
+std::string input;
+std::getline(std::cin, input);
+std::cin.clear();  // clearing the input buffer
+
+// now using the same logic as before, hoping it'll work out
+std::stringstream hugeStream(input);
+std::string firstSplit;  // this is the will split it at the ']' character
+
+std::getline(hugeStream, firstSplit, ']');  // get the first part of the string
+hugeStream >> carrier1.functionType >> carrier1.omega; // Carrier is what's left in hugeStream
+std::string firstPart; // these will hold the two parts of the string
+ // split at the ']' character
+
+
+// now we have the two parts, we can process them
+
+
+
+
+
+// now we need to process the first part to get the tones
+// removing the 'Ac [ ' part
+
+std::stringstream hugeStream2(firstSplit);
+std::string gettingAc;  // getting the value of Ac from the firstPart
+std::getline(hugeStream2, gettingAc, '['); 
+std::stringstream toneStream2(gettingAc);
+toneStream2 >> carrier1.amplitude; // Get Ac from the part before '['
+
+std::string insideTheBrackets;  // this will hold the inside of the brackets
+
+std::getline(hugeStream2, insideTheBrackets);
+
+// now we have the inside of the brackets, we can process it to get the tones
+
+std::stringstream hugeStream3(insideTheBrackets);
+std::string individualTones;
+
+int i=0; // since it needs to loop through the bracket four times
+int carrierConstant=1; // to skip the first part which is always 1
+while(std::getline(hugeStream3,individualTones,'+') && i<4){
+    
+    std::stringstream toneStream3(individualTones);
+
+    if(i==0){
+        carrierConstant=1; // just to skip the first part
+    }
+    else if(i==1){
+        toneStream3>>tone1_1.amplitude>>tone1_1.functionType>>tone1_1.omega;
+    }
+    else if(i==2){
+        toneStream3>>tone2_1.amplitude>>tone2_1.functionType>>tone2_1.omega;
+    }
+    else{
+     std::cerr<<"Invalid input\n";     // do nothing
+     std::cout<<"Exiting...\n";
+     std::cin.ignore();
+     exit(0);
+    }
+
+i++;
+}
+
+
+
+}
+
+
+
+
+void display1(){           // manually create this to display the 2 tone version, since trying to make it dynamic will be tedious
+
+
+    float u1_1 = tone1_1.amplitude;
+    float u2_1 = tone2_1.amplitude;
+    //float u3_1 = tone3_1.amplitude;
+
+    float modulationIndex = sqrt((u1_1*u1_1)+(u2_1*u2_1));
+    float carrierPower =(carrier1.amplitude*carrier1.amplitude)/2.f;
+    float sideBandPower = (carrierPower/2.f)*(modulationIndex*modulationIndex);
+    float totalPower = carrierPower + sideBandPower;
+    float efficiency = (sideBandPower/totalPower) * 100.f ;   
+
+
+    std::cout<<"Just to be sure! this is the wave\n";  // displaying the wave
+    std::cout<<"AM= "<<carrier1.amplitude<<" [ 1 + "<<tone1_1.amplitude<<"*"<<tone1_1.functionType<<"*"<<tone1_1.omega<<"t + "<<tone2_1.amplitude<<"*"<<tone2_1.functionType<<"*"<<tone2_1.omega<<"t ] * "<<carrier1.functionType<<"*"<<carrier1.omega<<"t\n";
+    
+    std::cout<<"\nThe various parameters are as follows:\n";
+    std::cout<<"Carrier Frequency: "<<(carrier1.omega/(2*3.14159))<<"\n";
+    std::cout<<"There are 4 bands in total, 2 USBs and 2 LSBs\n";
+    std::cout<<"Modulating index of the first tone: "<<tone1_1.amplitude<<"\n";
+    std::cout<<"Modulating index of the second tone: "<<tone2_1.amplitude<<"\n";
+    //std::cout<<"Modulating index of the third tone: "<<tone3_1.amplitude<<"\n";
+    std::cout<<"Total Modulating index: "<<modulationIndex<<"\n";
+    std::cout<<"Carrier Power: "<<carrierPower<<" W\n";
+    std::cout<<"Sideband Power: "<<sideBandPower<<" W\n";
+    std::cout<<"Total Power: "<<totalPower<<" W\n";
+    std::cout<<"Efficiency: "<<efficiency<<" %\n";
+
+    // now on the various bands
+    std::cout<<"The various bands are as follows:\n";
+    std::cout<<"Frequency of USB1: "<<(carrier.omega/(2*3.14159))+(tone1.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of LSB1: "<<(carrier.omega/(2*3.14159))-(tone1.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of USB2: "<<(carrier.omega/(2*3.14159))+(tone2.omega/(2*3.14159))<<"\n";
+    std::cout<<"Frequency of LSB2: "<<(carrier.omega/(2*3.14159))-(tone2.omega/(2*3.14159))<<"\n";
+    //std::cout<<"Frequency of USB3: "<<(carrier.omega/(2*3.14159))+(tone3.omega/(2*3.14159))<<"\n";
+    //std::cout<<"Frequency of LSB3: "<<(carrier.omega/(2*3.14159))-(tone3.omega/(2*3.14159))<<"\n";
+
+std::cin.clear();
+std::cin.get();
+
+}
+
+
+
+
+
+};
+
+
+
+
+
+
+
 class Calculation{
 
 // i want to find a new approach for this to work out using my own basic logic and understanding
@@ -336,7 +597,7 @@ int main(){
 
 std::cout<<"What do you wanna do?,\n";
 std::cout<<"1. Visualize a carrier and a modulating signal and their modulated signal\n";
-std::cout<<"2. Calculate the parameters of an AM signal of the format: A sin Wct + u sin Wmt * sin Wct\n";
+std::cout<<"2. Calculate \n";
 int choice;
 std::cin>>choice;
 std::cin.ignore();
@@ -427,10 +688,60 @@ while(window.isOpen()){
 
 
 else if(choice==2){
-    Calculation calculation;
+    std::cout<<"What is the format of the signal.\n";
+    std::cout<<"1. With a single tone modulating signal\n";
+    std::cout<<"2.  With a multi tone modulating signal\n";
+    int choice;
+    std::cin>>choice;
+    std::cin.ignore();
+
+    if(choice==1){
+        Calculation calculation;
     calculation.getModulatedSignal();
     calculation.display();
     calculation.calculatedParameters();
+
+    }
+    else if(choice==2){
+        std::cout<<"What's the format of the AM wave: ?\n";
+        std::cout<<"1. Ac [ 1 + u1 func W1t + u2 func W2t ] func Wct\n";
+        std::cout<<"2. Ac [ 1 + u1 func W1t + u2 func W2t + u3 func W3t ] func Wct  \n";
+        int ichoice;
+        std::cin>>ichoice;
+        std::cin.ignore();
+        if(ichoice==1){
+            multipleCalculation signal;
+            signal.getInput1();
+            signal.display1();
+        }
+        else if(ichoice==2){
+            multipleCalculation signal;
+        signal.getInput();
+        signal.display();
+        }
+        else{
+            std::cerr<<"Invalid choice\n";
+            std::cout<<"Exiting program...\n";
+            
+            std::cin.get();
+
+            
+        }
+        
+        
+
+    }
+    else{
+        std::cerr<<"Invalid choice\n";
+        std::cout<<"Exiting program...\n";
+        
+        std::cin.get();
+
+        
+    }
+
+
+    
 
 }
 
